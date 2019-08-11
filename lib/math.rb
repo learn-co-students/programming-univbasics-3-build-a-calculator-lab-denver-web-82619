b01d12b0baa3dd75305062f723a9d4901ac92c9a
Arlene George <aemg@live.com>
2019-08-11 20:14:58 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,26 @@
 def addition(num1, num2)
-
+  return num1+num2
 end
 
 def subtraction(num1, num2)
-
+  return num1-num2
 end
 
 def division(num1, num2)
-
+  return num1/num2
 end
 
 def multiplication(num1, num2)
-
+  return num1*num2
 end
 
 def modulo(num1, num2)
-
+decimal_answer= num1/num2
+round_low = decimal_answer.floor
+math_number = round_low * num2
+remainder = num1-math_number
 end
 
 def square_root(num)
-
+Math.sqrt num
 end
